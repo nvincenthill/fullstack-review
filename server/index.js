30dd60ffb27afc00c927fa044b180df49df418f8
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-14 10:44:30 -0700

    Fix gitignore bug

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ let app = express();
 
 // parse application/json
 app.use(parser.json());
-app.use(express.static(__dirname + "../client/dist"));
+app.use(express.static(__dirname + "/../client/dist"));
 
 app.post("/repos", function(req, res) {
   // This route should take the github username provided
